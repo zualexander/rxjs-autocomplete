37d52d0471f8cf7dd5bdd7f7d61c3d848796f8d6
Alexander Zulechner <alexanderzulechner@craftworks.at>
2017-04-02 22:22:58 +0200

    refactor: push autocomplete into separate file and call it only in index.js

---
+++ src/autocomplete.js
@@ -0,0 +1,40 @@
+import {Observable} from 'rxjs';
+
+export const activateAutoComplete = () => {
+    const $input = document.getElementById('textinput');
+    const $output = document.getElementById('output');
+
+    const keyboard$ = Observable.fromEvent($input, 'keyup')
+        .map(x => x.target.value)
+        .debounceTime(200)
+        .filter(text => text.length > 2)
+        .distinctUntilChanged();
+
+    keyboard$.subscribe((value) => {
+        const request$ = createRequestObservable(value);
+        const requestSubscribe = request$.subscribe(createAutocomplete, handleError);
+    });
+
+    function createRequestObservable(queryString) {
+        return Observable.ajax(`https://api.github.com/search/users?q=${queryString}`)
+            .map((response) => {
+                return response.response.items;
+            });
+    }
+
+    function createAutocomplete(data) {
+        console.dir(data);
+        while ($output.hasChildNodes()) {
+            $output.removeChild($output.lastChild);
+        }
+        data.forEach((user) => {
+            const $li = document.createElement('li');
+            $li.appendChild(document.createTextNode(user.login));
+            $output.appendChild($li);
+        });
+    }
+
+    function handleError(err) {
+        console.error(err);
+    }
+}
