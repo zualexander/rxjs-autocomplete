debf978cb719329a7f0d0254dc03ab2ac2bb434b
Alexander Zulechner <alexanderzulechner@craftworks.at>
2017-04-02 22:04:33 +0200

    feat(autocomplete): brings list of requested users

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 import {Observable} from 'rxjs';
 
+let inputString = '';
 const $input = document.getElementById('textinput');
-console.info("input", $input);
+const $output = document.getElementById('output');
+
 const keyboard$ = Observable.fromEvent($input, 'keyup')
     .map(x => x.target.value)
-    .debounceTime(700)
+    .debounceTime(200)
     .filter(text => text.length > 2)
     .distinctUntilChanged();
 
 keyboard$.subscribe((value) => {
-    console.info(value);
+    const request$ = createRequestObservable(value);
+    const requestSubscribe = request$.subscribe(createAutocomplete, handleError);
 });
+
+function createRequestObservable(queryString) {
+    return Observable.ajax(`https://api.github.com/search/users?q=${queryString}`)
+        .map((response) => {
+            return response.response.items;
+        });
+}
+
+function createAutocomplete(data) {
+    console.dir(data);
+    while ($output.hasChildNodes()) {
+        $output.removeChild($output.lastChild);
+    }
+    data.forEach((user) => {
+        const $li = document.createElement('li');
+        $li.appendChild(document.createTextNode(user.login));
+        $output.appendChild($li);
+    });
+}
+
+function handleError(err) {
+    console.error(err);
+}
