0f730d88137a9c69ef552f7413cc3a82bb466be3
Alexander Zulechner <alexanderzulechner@craftworks.at>
2017-04-03 09:31:44 +0200

    feat websocket creation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import {activateAutoComplete} from './autocomplete';
 
-activateAutoComplete();
+//activateAutoComplete();
+const connection = new WebSocket('ws://html5rocks.websocket.org/echo', ['soap', 'xmpp']);
+
+
+// When the connection is open, send some data to the server
+connection.onopen = function () {
+    connection.send('Ping'); // Send the message 'Ping' to the server
+};
+
+// Log errors
+connection.onerror = function (error) {
+    console.error(error);
+};
+
+// Log messages from the server
+connection.onmessage = function (e) {
+    console.log('Server: ' + e.data);
+};
